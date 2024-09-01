f18cbfcc86fcbdf8a73fc1fa2d4bd9e2a6af385a
Verolni <veronicatorrenegra@gmail.com>
2024-09-01 17:24:53 -0500

    Create controller for course

--- src/main/java/org/riwi/Spring_Workshop_Week_3/controllers/ImplementationControllers/CourseController.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/controllers/ImplementationControllers/CourseController.java
@@ -1,4 +1,32 @@
 package org.riwi.Spring_Workshop_Week_3.controllers.ImplementationControllers;
 
+import org.riwi.Spring_Workshop_Week_3.dtos.Response.CourseResponseDTO;
+import org.riwi.Spring_Workshop_Week_3.service.InterfacesPerEntity.InterfaceCourseService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class CourseController {
+
+    private InterfaceCourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<Page<CourseResponseDTO>> getCoursesPaginated(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        Page<CourseResponseDTO> coursePage = courseService.getCoursesPaginated(page, size);
+        return new ResponseEntity<>(coursePage, HttpStatus.OK);
+    }
+    @GetMapping("/search")
+    public ResponseEntity<Page<CourseResponseDTO>> searchCourses(
+            @RequestParam("keyword") String keyword,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        Page<CourseResponseDTO> coursePage = courseService.searchCourses(keyword, page, size);
+        return new ResponseEntity<>(coursePage, HttpStatus.OK);
+    }
+
+
 }
