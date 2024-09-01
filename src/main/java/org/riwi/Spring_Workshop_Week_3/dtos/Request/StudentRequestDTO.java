46bf1dfe6bfc15ef61b982b4ec238ee775fe2ef7
Julian Alvarez <juliansnei@gmail.com>
2024-09-01 11:43:21 -0500

    To add annotations to the studentRequestDTO

--- src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Request/StudentRequestDTO.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Request/StudentRequestDTO.java
@@ -1,5 +1,14 @@
 package org.riwi.Spring_Workshop_Week_3.dtos.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentRequestDTO {
     private String name;
     private String email;
