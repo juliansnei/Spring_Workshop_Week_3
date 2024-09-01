a2e6ef0adb9f1520ed186e10e430cdd6df826462
Jonathan <josuarezp@unal.edu.co>
2024-09-01 10:43:35 -0500

    creation of implement

--- src/main/java/org/riwi/Spring_Workshop_Week_3/service/ImplementationServices/StudentServiceImpl.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/service/ImplementationServices/StudentServiceImpl.java
@@ -1,4 +1,28 @@
 package org.riwi.Spring_Workshop_Week_3.service.ImplementationServices;
 
-public class StudentServiceImpl {
+import org.riwi.Spring_Workshop_Week_3.dtos.Response.StudentResponseDTO;
+import org.riwi.Spring_Workshop_Week_3.service.InterfacesPerEntity.InterfaceStudentService;
+
+import java.util.List;
+
+public class StudentServiceImpl implements InterfaceStudentService {
+    @Override
+    public StudentResponseDTO update(Long aLong, StudentResponseDTO studentResponseDTO) {
+        return null;
+    }
+
+    @Override
+    public List<StudentResponseDTO> readAll() {
+        return List.of();
+    }
+
+    @Override
+    public StudentResponseDTO readById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public StudentResponseDTO save(StudentResponseDTO studentResponseDTO) {
+        return null;
+    }
 }
