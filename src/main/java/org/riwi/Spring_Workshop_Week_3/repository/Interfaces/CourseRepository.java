1cd164e3ab7027156320d6a3b8e074788474c931
Verolni <veronicatorrenegra@gmail.com>
2024-09-01 16:24:56 -0500

    Create Repository of course

--- src/main/java/org/riwi/Spring_Workshop_Week_3/repository/Interfaces/CourseRepository.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/repository/Interfaces/CourseRepository.java
@@ -2,6 +2,16 @@ package org.riwi.Spring_Workshop_Week_3.repository.Interfaces;
 
 import org.riwi.Spring_Workshop_Week_3.entities.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
+
+
+    @Query("SELECT c FROM CourseEntity c WHERE c.name LIKE %:keyword%")
+    List<CourseEntity> searchByKeyword(@Param("keyword") String keyword);
+
+
 }
