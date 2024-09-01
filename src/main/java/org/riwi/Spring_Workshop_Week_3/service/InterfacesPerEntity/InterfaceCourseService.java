e750b56e6526d4fc6a0c4ba5f03c10bdb959fa56
Verolni <veronicatorrenegra@gmail.com>
2024-09-01 16:28:04 -0500

    Create interface service of course

--- src/main/java/org/riwi/Spring_Workshop_Week_3/service/InterfacesPerEntity/InterfaceCourseService.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/service/InterfacesPerEntity/InterfaceCourseService.java
@@ -1,4 +1,12 @@
 package org.riwi.Spring_Workshop_Week_3.service.InterfacesPerEntity;
 
+import org.riwi.Spring_Workshop_Week_3.dtos.Response.CourseResponseDTO;
+import org.springframework.data.domain.Page;
+
 public interface InterfaceCourseService {
+
+    Page<CourseResponseDTO> getCoursesPaginated(int page, int size);
+
+    Page<CourseResponseDTO> searchCourses(String keyword, int page, int size);
+
 }
