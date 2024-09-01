043ca89fc58ad2335afc3f4cb33d9f9927bf9855
Verolni <veronicatorrenegra@gmail.com>
2024-09-01 16:19:55 -0500

    Create DTO response of course

--- src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Response/CourseResponseDTO.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Response/CourseResponseDTO.java
@@ -1,4 +1,16 @@
 package org.riwi.Spring_Workshop_Week_3.dtos.Response;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class CourseResponseDTO {
+
+    private Long id;
+    private String name;
+    private List<Long> studentIds;
+    private List<Long> lessonIds;
+
+
 }
