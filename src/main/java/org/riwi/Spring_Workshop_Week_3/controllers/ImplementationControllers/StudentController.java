9f345d5ffc6727ced8b1070153b11da0591c7a4e
Julian Alvarez <juliansnei@gmail.com>
2024-09-01 11:57:21 -0500

    Adding anotations inc StudentController

--- src/main/java/org/riwi/Spring_Workshop_Week_3/controllers/ImplementationControllers/StudentController.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/controllers/ImplementationControllers/StudentController.java
@@ -1,7 +1,7 @@
 package org.riwi.Spring_Workshop_Week_3.controllers.ImplementationControllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class StudentController {
-
-
-
 }
