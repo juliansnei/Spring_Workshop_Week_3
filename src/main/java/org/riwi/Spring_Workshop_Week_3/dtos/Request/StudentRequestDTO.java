170712d3607eea6782c7a2cb04b4dda9199280b7
Julian Alvarez <juliansnei@gmail.com>
2024-09-01 10:22:06 -0500

    Interface working

--- src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Request/StudentRequestDTO.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/dtos/Request/StudentRequestDTO.java
@@ -1,4 +1,6 @@
 package org.riwi.Spring_Workshop_Week_3.dtos.Request;
 
 public class StudentRequestDTO {
+    private String name;
+    private String email;
 }
--- src/main/java/org/riwi/Spring_Workshop_Week_3/service/InterfacesPerEntity/InterfaceStudentService.java
+++ src/main/java/org/riwi/Spring_Workshop_Week_3/service/InterfacesPerEntity/InterfaceStudentService.java
@@ -1,4 +1,10 @@
 package org.riwi.Spring_Workshop_Week_3.service.InterfacesPerEntity;
 
-public interface InterfaceStudentService {
+import org.riwi.Spring_Workshop_Week_3.dtos.Response.StudentResponseDTO;
+import org.riwi.Spring_Workshop_Week_3.entities.StudentEntity;
+
+public interface InterfaceStudentService
+
+                ReadAll<StudentResponseDTO, StudentEntity>
+    {
 }
